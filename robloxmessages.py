0f797b059c6b2132638ea64112d7d505088e695c
GooseBotBetaTesting <goosebot.betatesting@gmail.com>
2021-10-10 19:52:21 +0200

    config, seperate file for cookie

--- robloxmessages.py
+++ robloxmessages.py
@@ -4,24 +4,28 @@ import time
 import win10toast
 from jsonc_parser.parser import JsoncParser
 
-cookie = JsoncParser.parse_file('./conf.jsonc').get("cookie")
+cookie = JsoncParser.parse_file('./cookie.jsonc').get("cookie")
+config = JsoncParser.parse_file('./config.jsonc')
 
-url = "https://privatemessages.roblox.com/v1/messages/unread/count"
-cookies = {'.ROBLOSECURITY': cookie}
+API_URL = "https://privatemessages.roblox.com/v1/messages/unread/count"
 
 def notify():
     toast = win10toast.ToastNotifier()
-    toast.show_toast("Message", "You got a new message!", duration=3, icon_path="./icon.ico", threaded=True, sound=False)
+    toast.show_toast("Message", "You got a new message!", duration=10, icon_path="./icon.ico", threaded=True, sound=False)
 
 while True:
-	urlHandler = requests.get(url, headers={"content-type": "application/json"}, cookies=cookies)
+	urlHandler = requests.get(API_URL,
+	    headers = {"content-type": "application/json"},
+	    cookies = {'.ROBLOSECURITY': cookie}
+	)
+
 	result = urlHandler.json()
 	
 	if result != None:
 		count = result.get('count')
 
 		if count != None:
-			if count >= 1:
+			if count >= config.get("minunread"):
 				notify()
 				time.sleep(60)
 			else:
