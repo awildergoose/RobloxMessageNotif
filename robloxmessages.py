88764298ad499f9025e69208008d138bae27d731
GooseBotBetaTesting <goosebot.betatesting@gmail.com>
2021-10-10 19:28:34 +0200

    Init

---
+++ robloxmessages.py
@@ -0,0 +1,31 @@
+import requests
+import time
+
+import win10toast
+from jsonc_parser.parser import JsoncParser
+
+cookie = JsoncParser.parse_file('./conf.jsonc').get("cookie")
+
+url = "https://privatemessages.roblox.com/v1/messages/unread/count"
+cookies = {'.ROBLOSECURITY': cookie}
+
+def notify():
+    toast = win10toast.ToastNotifier()
+    toast.show_toast("Message", "You got a new message!", duration=3, icon_path="./icon.ico", threaded=True, sound=False)
+
+while(True):
+	urlHandler = requests.get(url, headers={"content-type": "application/json"}, cookies=cookies)
+	result = urlHandler.json()
+	
+	if(result != None):
+		count = result.get('count')
+
+		if(count != None):
+			if(count >= 1):
+				notify()
+				time.sleep(60)
+			else:
+				time.sleep(25)
+		else:
+			print("Ratelimited or not logged in.")
+			time.sleep(60)
