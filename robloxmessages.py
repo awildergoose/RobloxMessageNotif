f3bd9c43c1f4033306fe34586eb83a8d1834404b
stev <steven.rafft@gmail.com>
2021-11-09 03:41:57 +0200

    tray icon

--- robloxmessages.py
+++ robloxmessages.py
@@ -1,10 +1,21 @@
+# Also make this an option
+# Tray icon using pystray
+# https://pystray.readthedocs.io/en/latest/usage.html
+# ##################Documentation####################
+
+import ctypes
+import pystray
 import colorama
 import webbrowser
 import requests
 import time
 
+from os import _exit as exit
+from os import getppid as getppid
+from threading import Thread
 from jsonc_parser.parser import JsoncParser
 from win10toast_click import ToastNotifier
+from PIL import Image
 
 cookie = JsoncParser.parse_file("./cookie.jsonc").get("cookie")
 config = JsoncParser.parse_file("./config.jsonc")
@@ -31,19 +42,44 @@ except:
 API_URL = "https://privatemessages.roblox.com/v1/messages/unread/count"
 PAGE_URL = "https://www.roblox.com/my/messages/#!/inbox"
 
+
+def on_tray_clicked(icon, item):
+    exit(0)
+
+
+def startTrayIcon():
+    trayIcon = pystray.Icon('Roblox Messsages Notifier', Image.open('./res/icons/default.ico'), menu=pystray.Menu(
+        pystray.MenuItem(
+            'Exit',
+            on_tray_clicked,
+        )))
+
+    trayIcon.run()
+
+
+if config.get("trayIconEnabled") == True:
+    Thread(target=startTrayIcon).start()
+
+if config.get("debug") == False:
+    ctypes.windll.user32.ShowWindow(
+        ctypes.windll.kernel32.GetConsoleWindow(), 0)
+
+
 def onClick():
     webbrowser.open_new(PAGE_URL)
+
 
 def notify():
     toast = ToastNotifier()
     toast.show_toast(themeDetails["msgTitle"], themeDetails["msgContent"], duration=themeDetails["msgDuration"],
                      icon_path=themeDetails["msgIcon"], threaded=True, callback_on_click=onClick)
 
+
 def sendRobloxRequest(URL):
     return requests.get(API_URL,
-                            headers={"content-type": "application/json"},
-                            cookies={".ROBLOSECURITY": cookie}
-                            ).json()
+                        headers={"content-type": "application/json"},
+                        cookies={".ROBLOSECURITY": cookie}
+                        ).json()
 
 
 while True:
